0797d8f3ad9e56f82d759722644816980d36c4bc
dreiver1 <viniciosdavid1@gmail.com>
2023-04-09 08:33:20 -0300

    concluindo implementacao

--- hash/EstruturaHashTable.java
+++ hash/EstruturaHashTable.java
@@ -2,6 +2,10 @@ package hash;
 public class EstruturaHashTable implements EstruturaDeDados{
 
     private Integer tabela[];
+    
+    public static void main(String[] args) {
+        System.out.println("bugado");
+    }
 
     public EstruturaHashTable() {
         tabela = new Integer[1000];
@@ -10,6 +14,11 @@ public class EstruturaHashTable implements EstruturaDeDados{
     @Override
     public boolean insert(int chave) {
         // TODO quando inserir, se a posição estiver ocupada, o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
+        int index = chave%this.tabela.length;
+        if(tabela[index] == null){
+            tabela[index] = chave;
+            return true;
+        }
         return false;
         
     }
@@ -17,6 +26,11 @@ public class EstruturaHashTable implements EstruturaDeDados{
     @Override
     public boolean delete(int chave) {
         // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
+        int index = chave%this.tabela.length;
+        if(tabela[index] != null){
+            tabela[index] = null;
+            return true;
+        }
         return false;
         
     }
@@ -24,6 +38,10 @@ public class EstruturaHashTable implements EstruturaDeDados{
     @Override
     public boolean search(int chave) {
         // TODO se a posição estiver ocupada e valor é o mesmo (ou seja, tem que ser ambos), retorna true. Caso contrário, retorna true.
+        int index = chave%this.tabela.length;
+        if(tabela[index] != null && tabela[index] == chave){
+            return true;
+        }
         return false;
     }
     
